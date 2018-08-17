aca6fe4dbff59d4d998d4dc5def45804ef4268b1
季颖达-豆包 <yingda.ji@baodanyun-inc.com>
2018-08-17 10:08:23 +0800

    init

--- src/DateIn.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-
-public class DateIn extends Pair<LocalDate> {
-    public void setSecond(LocalDate second){
-        if(second.compareTo(getFirst()) >= 0){
-            super.setSecond(second);
-        }
-    }
-}
