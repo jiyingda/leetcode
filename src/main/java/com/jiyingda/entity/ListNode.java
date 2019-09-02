2608c9e3e2b23e127e1347b2b9703e91e97e266d
jiyingda <jiyingda@vipkid.com.cn>
2019-09-02 17:15:30 +0800

    0902

---
+++ src/main/java/com/jiyingda/entity/ListNode.java
@@ -0,0 +1,13 @@
+package com.jiyingda.entity;
+
+
+/**
+ * @author jiyingda.
+ * @date 2019/9/2.
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
