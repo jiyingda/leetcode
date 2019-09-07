dbb73f21a649bb6dab443a42365b62acfe55f0d6
jiyingda <610181296@qq.com>
2019-09-07 23:20:20 +0800

    28

---
+++ src/main/java/com/jiyingda/Leetcode/Leet16.java
@@ -0,0 +1,90 @@
+package com.jiyingda.Leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author jiyingda
+ * @date 2019/8/10
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ *
+ * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+ *
+ * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum-closest
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leet16 {
+
+    public static void main(String[] args){
+
+        int[] numx = new int[]{-1, 2, 1, -4};
+        System.out.println(threeSumClosest(numx, -5));;
+
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+        int len = nums.length;
+        if(len <= 3){
+            int re = 0;
+            for(int n : nums){
+                re += n;
+            }
+            return re;
+        }
+        Arrays.sort(nums);
+        if(nums[0] + nums[1] + nums[2] >= target){ return nums[0] + nums[1] + nums[2];}
+        if(nums[len-1] + nums[len-2] + nums[len-3] <= target){
+            return nums[len-1] + nums[len-2] + nums[len-3];
+        }
+        Integer min = null;
+        Integer max = null;
+        for(int i = 0; i < len; i++){
+            for(int j = i + 1; j < len; j++){
+                int ab = nums[i] + nums[j];
+                for(int k = len - 1; k > j; k--){
+                    int abc = ab + nums[k];
+                    if(abc == target){
+                        return abc;
+                    }
+                    if(abc < target){
+                        if(min == null || min < abc){
+                            min = abc;
+                        }
+                        break;
+                    }
+
+                    if(max == null || max > abc){
+                        max = abc;
+                    }
+                    /** 跳过重复的 */
+                    while (k > j && nums[k] == nums[k - 1]){
+                        k--;
+                    }
+                }
+                /** 跳过重复的 */
+                while (j + 1 < len && nums[j] == nums[j + 1]){
+                    j++;
+                }
+            }
+            /** 跳过重复的 */
+            while (i + 1 < len && nums[i] == nums[i + 1]){
+                i++;
+            }
+
+        }
+        if(min == null){
+            return max;
+        }
+        if(max == null){
+            return min;
+        }
+        if((target - min) < (max - target)){
+            return min;
+        } else {
+            return max;
+        }
+
+    }
+}
---
+++ src/main/java/com/jiyingda/Leetcode/Leet342.java
@@ -0,0 +1,54 @@
+package com.jiyingda.Leetcode;
+
+/**
+ * @author jiyingda.
+ * @date 2019/7/29.
+ *
+ *
+ * 给定一个整数 (32 位有符号整数)，请编写一个函数来判断它是否是 4 的幂次方。
+ *
+ * 示例 1:
+ *
+ * 输入: 16
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: 5
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/power-of-four
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Leet342 {
+
+    public static void main(String[] args){
+
+        int s = 17;
+        System.out.println(s << 2);;
+        System.out.println( 17 * 4);
+        System.out.println(isPowerOfFour(16));
+    }
+
+    public static boolean isPowerOfFour(int num) {
+        if(num == 1){
+            return true;
+        }
+        if(num == 4){
+            return true;
+        }
+        if(num < 16){
+            return false;
+        }
+        do {
+            int tmp = num;
+            num = num >>> 2;
+            if(tmp != (num << 2)){
+                return false;
+            }
+        } while (num >= 4);
+        return num == 1;
+
+    }
+}
