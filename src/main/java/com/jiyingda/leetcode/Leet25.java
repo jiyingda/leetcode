f78eba25b5a409402668c957b03789f7a4807ddc
jiyingda <jiyingda@vipkid.com.cn>
2019-10-24 20:59:28 +0800

    25

---
+++ src/main/java/com/jiyingda/leetcode/Leet25.java
@@ -0,0 +1,29 @@
+package com.jiyingda.leetcode;
+
+/**
+ * @author jiyingda.
+ * @date 2019/10/24.
+ * 给定这个链表：1->2->3->4->5
+ *
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ *
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ *
+ * 说明 :
+ *
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leet25 {
+
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+
+        //TODO 25
+        return null;
+    }
+}
