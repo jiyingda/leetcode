28f15e0dd36a8323ef93fdc121d7c87a7b0b241b
jiyingda <jiyingda@vipkid.com.cn>
2020-03-27 16:35:46 +0800

    50

---
+++ src/main/java/com/jiyingda/leetcode/Leet49.java
@@ -0,0 +1,73 @@
+package com.jiyingda.leetcode;
+
+import java.util.*;
+
+/**
+ * @author jiyingda.
+ * @date 2020/3/26.
+ *
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 示例:
+ *
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * 说明：
+ *
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leet49 {
+
+
+    public static void main(String[] args){
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> re = groupAnagrams(strs);
+        System.out.println(re);
+    }
+
+
+
+
+
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+
+        List<List<String>> reList = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+
+
+        for(String s : strs){
+            char[] chs = s.toCharArray();
+
+                Arrays.sort(chs);
+                String sss = String.valueOf(chs);
+                if(map.containsKey(sss)){
+                    map.get(sss).add(s);
+                } else {
+                    List<String> tmp = new ArrayList<>();
+                    tmp.add(s);
+                    map.put(sss, tmp);
+                }
+
+
+        }
+
+        /*for(Map.Entry<String, List<String>> en : map.entrySet()){
+            reList.add(en.getValue());
+        }*/
+        return new ArrayList<>(map.values());
+
+    }
+}
