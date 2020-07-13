b182f6b8f66af00b852aae6d31a51303ec6cd058
jiyingda <jiyingda@vipkid.com.cn>
2020-07-13 17:19:45 +0800

    72

--- src/main/java/com/jiyingda/leetcode100/Leet72.java
+++ src/main/java/com/jiyingda/leetcode100/Leet72.java
@@ -42,7 +42,7 @@ public class Leet72 {
 
     public static void main(String[] args){
 
-        System.out.print(minDistance("intention", "execution"));
+        System.out.print(minDistance("horse", "ros"));
     }
 
 
@@ -50,27 +50,39 @@ public class Leet72 {
     public static int minDistance(String word1, String word2) {
 
 
-        return findLCS(word1, word1.length(), word2, word2.length());
+        int n = word1.length();
+        int m = word2.length();
+
+        // 有一个字符串为空串
+        if (n * m == 0)
+            return n + m;
+
+        // DP 数组
+        int [][] D = new int[n + 1][m + 1];
+
+        // 边界状态初始化
+        for (int i = 0; i < n + 1; i++) {
+            D[i][0] = i;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            D[0][j] = j;
+        }
+
+        // 计算所有 DP 值
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                int left = D[i - 1][j] + 1;
+                int down = D[i][j - 1] + 1;
+                int left_down = D[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1))
+                    left_down += 1;
+                D[i][j] = Math.min(left, Math.min(down, left_down));
+
+            }
+        }
+        return D[n][m];
     }
 
 
-    public static int findLCS(String word1, int n, String word2, int m) {
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                dp[i][j] = 0;
-            }
-        }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        return dp[n][m];
-    }
 
 }
