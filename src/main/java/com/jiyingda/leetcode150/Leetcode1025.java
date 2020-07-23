bb9017f3ea5d8540629171e1ba82b2da6f399a94
jiyingda <jiyingda@vipkid.com.cn>
2020-07-23 14:31:51 +0800

    1025

---
+++ src/main/java/com/jiyingda/leetcode150/Leetcode1025.java
@@ -0,0 +1,33 @@
+package com.jiyingda.leetcode150;
+
+/**
+ * @author jiyingda.
+ * @date 2020/7/23.
+ */
+public class Leetcode1025 {
+
+    public static void main(String[] args){
+
+    }
+    public boolean divisorGame(int N) {
+        boolean[] dp = new boolean[N + 1];
+        if(N <= 1){
+           return false;
+        }
+        dp[1] = false;
+        dp[2] = true;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = false;
+
+            for (int x = 1; x < i; x++) {
+                if (i % x == 0 && !dp[i-x]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[N];
+    }
+
+}
