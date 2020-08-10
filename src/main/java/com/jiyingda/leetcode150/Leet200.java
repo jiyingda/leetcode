1eecf2533b997bead695def528902624a73527c8
jiyingda <jiyingda@vipkid.com.cn>
2020-08-10 20:25:34 +0800

    200

---
+++ src/main/java/com/jiyingda/leetcode150/Leet200.java
@@ -0,0 +1,41 @@
+package com.jiyingda.leetcode150;
+
+/**
+ * @author jiyingda.
+ * @date 2020/8/10.
+ *
+ * 在整数数组 nums 中，是否存在两个下标 i 和 j，使得 nums [i] 和 nums [j] 的差的绝对值小于等于 t ，且满足 i 和 j 的差的绝对值也小于等于 ķ 。
+ *
+ * 如果存在则返回 true，不存在返回 false。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,2,3,1], k = 3, t = 0
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: nums = [1,0,1,1], k = 1, t = 2
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: nums = [1,5,9,1,5,9], k = 2, t = 3
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leet200 {
+
+    public static void main(String[] args){
+
+    }
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+
+        return false;
+    }
+}
