d596d194552bb1519692c00bf9ae72fed1067c42
jiyingda <jiyingda@qq.com>
2021-01-05 00:25:35 +0800

    509

---
+++ src/main/java/com/jiyingda/offer/Offer06.java
@@ -0,0 +1,60 @@
+package com.jiyingda.offer;
+
+import com.jiyingda.entity.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ *  
+ *
+ * 限制：
+ *
+ * 0 <= 链表长度 <= 10000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jiyingda
+ * @date 2020/12/21 13:24
+ */
+public class Offer06 {
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        n1.next = n2;
+        n2.next = n3;
+        int[] ints = reversePrint(null);
+        for(int i : ints){
+            System.out.print(i + "->");
+        }
+    }
+
+
+    public static int[] reversePrint(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        int[] re = new int[list.size()];
+        for(int i = re.length-1,j=0; i >= 0; i--,j++){
+            re[i] = list.get(j);
+        }
+        return re;
+    }
+}
