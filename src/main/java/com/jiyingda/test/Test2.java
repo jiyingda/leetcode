f2d9a54d28fea2173b6830dd1e2a32be981f7d78
jiyingda <jiyingda@fenbi.com>
2021-01-20 19:55:56 +0800

    dev

---
+++ src/main/java/com/jiyingda/test/Test2.java
@@ -0,0 +1,20 @@
+package com.jiyingda.test;
+
+/**
+ * @author jiyingda
+ * @date 2021/1/20
+ */
+public class Test2 {
+
+
+    public static void main(String[] args){
+        int a = 500000000;
+        int d = a / 50;
+        int b = d/2000;
+        int c = b/60;
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
