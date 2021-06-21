dc4f4e81678c076bd83e5616691a5be89fb112b3
jiyingda <610181296@qq.com>
2021-06-21 23:05:01 +0800

    ADD: 235

---
+++ src/main/java/com/jiyingda/leetcode200/Leet235.java
@@ -0,0 +1,83 @@
+package com.jiyingda.leetcode200;
+
+import com.jiyingda.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ *
+ *
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * 示例 2:
+ *
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ *  
+ *
+ * 说明:
+ *
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉搜索树中。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jiyingda
+ * @date 2021/1/27
+ */
+public class Leet235 {
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(6);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(8);
+        TreeNode n4 = new TreeNode(0);
+        TreeNode n5 = new TreeNode(4);
+        TreeNode n6 = new TreeNode(3);
+        TreeNode n7 = new TreeNode(5);
+        TreeNode n8 = new TreeNode(7);
+        TreeNode n9 = new TreeNode(9);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n5.left = n6;
+        n5.right = n7;
+        n3.left = n8;
+        n3.right = n9;
+        TreeNode node = lowestCommonAncestor(n1, n2, n5);
+        System.out.print(node.val);
+
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ancestor = root;
+        while (true) {
+            if (p.val < ancestor.val && q.val < ancestor.val) {
+                ancestor = ancestor.left;
+            } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                ancestor = ancestor.right;
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+
+}
