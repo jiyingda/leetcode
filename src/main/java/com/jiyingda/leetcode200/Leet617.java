6a8e765ed017c2869085cfe653044cf1c87f68c9
jiyingda <jiyingda@fenbi.com>
2021-08-01 18:41:26 +0800

    617

--- src/main/java/com/jiyingda/leetcode200/Leet617.java
+++ src/main/java/com/jiyingda/leetcode200/Leet617.java
@@ -1,9 +1,81 @@
 /**
-* @(#)Leet617.java, 8月 01, 2021.
-*
-* Copyright 2021 fenbi.com. All rights reserved.
-* FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
-*/package com.jiyingda.leetcode200;/**
-* @author jiyingda
-*/public class Leet617 {
+ * @(#)Leet617.java, 8月 01, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.leetcode200;
+
+/**
+ *
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ *
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * 	Tree 1                     Tree 2
+ *           1                         2
+ *          / \                       / \
+ *         3   2                     1   3
+ *        /                           \   \
+ *       5                             4   7
+ * 输出:
+ * 合并后的树:
+ * 	     3
+ * 	    / \
+ * 	   4   5
+ * 	  / \   \
+ * 	 5   4   7
+ * 注意: 合并必须从两个树的根节点开始。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-binary-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jiyingda
+ */
+public class Leet617 {
+
+    public static void main(String[] args) {
+        TreeNode root14 = new TreeNode(5);
+        TreeNode root13 = new TreeNode(3, root14, null);
+        TreeNode root12 = new TreeNode(2);
+        TreeNode root11 = new TreeNode(1, root13, root12);
+
+        TreeNode root24 = new TreeNode(4);
+        TreeNode root27 = new TreeNode(7);
+        TreeNode root21 = new TreeNode(1, null, root24);
+        TreeNode root23 = new TreeNode(3, null, root27);
+        TreeNode root22 = new TreeNode(2, root21, root23);
+
+        TreeNode treeNode = mergeTrees(root11, root22);
+        System.out.println(treeNode);
+    }
+
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+           return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
 }
+class TreeNode {
+    int val;
+    TreeNode left;
+     TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+         this.left = left;
+        this.right = right;
+     }
+ }
