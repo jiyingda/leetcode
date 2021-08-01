591d04cb10cde897d5ba3e76b4a62d4b939effd7
jiyingda <jiyingda@fenbi.com>
2021-08-01 20:32:45 +0800

    217

---
+++ src/main/java/com/jiyingda/leetcode200/Leet217.java
@@ -0,0 +1,56 @@
+/**
+ * @(#)Leet217.java, 8月 01, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.leetcode200;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jiyingda
+ */
+public class Leet217 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int a : nums) {
+            if (set.contains(a)) {
+                return true;
+            } else {
+                set.add(a);
+            }
+        }
+        return false;
+    }
+}
