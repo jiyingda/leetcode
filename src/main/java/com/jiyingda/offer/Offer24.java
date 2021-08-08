0be217f362ee56a001ecede95e22db9d4249e3bd
jiyingda <jiyingda@fenbi.com>
2021-08-08 18:26:09 +0800

    ADD: offer24

---
+++ src/main/java/com/jiyingda/offer/Offer24.java
@@ -0,0 +1,82 @@
+/**
+ * @(#)Offer24.java, 8月 08, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.offer;
+
+/**
+ *
+ * 给定单链表的头节点 head ，请反转链表，并返回反转后的链表的头节点。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 示例 3：
+ *
+ * 输入：head = []
+ * 输出：[]
+ *  
+ *
+ * 提示：
+ *
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ *  
+ *
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/UHnkqh
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jiyingda
+ */
+public class Offer24 {
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2, n1);
+        ListNode n3 = new ListNode(3, n2);
+        ListNode n4 = new ListNode(4, n3);
+        ListNode n5 = new ListNode(5, n4);
+        ListNode ll = reverseList(n2);
+        System.out.println(ll);
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode idx = new ListNode();
+        idx.next = head;
+        while (head.next != null) {
+            ListNode next = head.next.next;
+            ListNode tmp = head.next;
+            head.next = null;
+            tmp.next = idx.next;
+            idx.next = tmp;
+            head.next = next;
+        }
+        return idx.next;
+    }
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
