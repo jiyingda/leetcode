b25c27376ade7cd0932f6941d49b7ba625fbda9c
jiyingda <jiyingda@fenbi.com>
2021-09-26 22:15:37 +0800

    test

--- src/main/java/com/jiyingda/test/Test2.java
+++ src/main/java/com/jiyingda/test/Test2.java
@@ -28,17 +28,18 @@ public class Test2 {
             System.out.println((i & 0xFF) >> 1);
         }*/
 
-        String sqla = "CREATE TABLE `user_city_";
+        String sqla = "CREATE TABLE `user_school_";
         String sqlb = "` (\n" +
-                "  `id` bigint(20) NOT NULL AUTO_INCREMENT,\n" +
+                "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',\n" +
                 "  `userId` bigint(20) NOT NULL COMMENT '用户Id',\n" +
-                "  `adCode` varchar(255) NOT NULL COMMENT '城市code',\n" +
+                "  `phaseId` tinyint(4) NOT NULL COMMENT '阶段id',\n" +
+                "  `schoolId` varchar(255) NOT NULL COMMENT '学校id',\n" +
                 "  `dbctime` datetime(3) DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',\n" +
                 "  `dbutime` datetime(3) DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '更新时间',\n" +
                 "  PRIMARY KEY (`id`),\n" +
-                "  UNIQUE KEY `uq` (`userId`),\n" +
+                "  UNIQUE KEY `uq_userId_phaseId` (`userId`, `phaseId`),\n" +
                 "  KEY `idx_dbctime` (`dbutime`)\n" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户学校';";
         System.out.println("## tutor_profile_shard0");
         int j = 0;
         List<String> list = new ArrayList<>();
@@ -46,7 +47,7 @@ public class Test2 {
         for (int i = 0; i < 128; i++) {
             if (i % 16 == 0) {
                 System.out.println("## tutor_profile_shard" + j);
-                String s = "tutor_profile_shard"+k+".user_city_${"+(k*16)+".."+((k+1)*16 -1)+"},";
+                String s = "tutor_profile_shard"+k+".user_school_${"+(k*16)+".."+((k+1)*16 -1)+"},";
                 k++;
                 list.add(s);
                 j++;
