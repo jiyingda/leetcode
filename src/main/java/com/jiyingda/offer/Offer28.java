8d2376ec80ec14c5c8662872bd1003d147a423d4
jiyingda <jiyingda@fenbi.com>
2021-10-12 12:41:53 +0800

    ADD: offer28

---
+++ src/main/java/com/jiyingda/offer/Offer28.java
@@ -0,0 +1,90 @@
+/**
+ * @(#)Offer29.java, 10月 12, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.offer;
+
+/**
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingda
+ */
+public class Offer28 {
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(2);
+        TreeNode n4 = new TreeNode(3);
+        TreeNode n5 = new TreeNode(3);
+        TreeNode n6 = new TreeNode(3);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n3.right = n5;
+        n5.right = n6;
+        System.out.println(isSymmetric(n1));
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return diff(root.left, root.right);
+    }
+
+    public static boolean diff(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if(left.val != right.val) {
+            return false;
+        }
+        boolean f = diff(left.left, right.right);
+        if (!f) {
+            return false;
+        }
+        return diff(left.right, right.left);
+    }
+
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+}
