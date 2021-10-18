bd53277f2973093e7c959685b2becb51d2d0a6c0
jiyingda <jiyingda@fenbi.com>
2021-10-18 23:13:38 +0800

    ADD: 15

---
+++ src/main/java/com/jiyingda/offer/Offer57.java
@@ -0,0 +1,53 @@
+/**
+ * @(#)Offer57.java, 10月 12, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.offer;
+
+/**
+ * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[2,7] 或者 [7,2]
+ * 示例 2：
+ *
+ * 输入：nums = [10,26,30,31,47,60], target = 40
+ * 输出：[10,30] 或者 [30,10]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingda
+ */
+public class Offer57 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,7,11,15};
+        int[] re = twoSum(nums, 9);
+        System.out.println(re[0] + ", " + re[1]);
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int base = nums[left] + nums[right];
+            if (base == target) {
+                return new int[]{nums[left], nums[right]};
+            }
+            if (base > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return null;
+    }
+}
