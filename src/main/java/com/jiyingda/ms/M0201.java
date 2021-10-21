ac0e571b7d3296d897f08d426d929f7a89f10097
jiyingda <jiyingda@fenbi.com>
2021-10-21 22:19:14 +0800

    ADD: m0201

---
+++ src/main/java/com/jiyingda/ms/M0201.java
@@ -0,0 +1,80 @@
+/**
+ * @(#)M0201.java, 10月 21, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.ms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+ *
+ * 示例1:
+ *
+ *  输入：[1, 2, 3, 3, 2, 1]
+ *  输出：[1, 2, 3]
+ * 示例2:
+ *
+ *  输入：[1, 1, 1, 1, 2]
+ *  输出：[1, 2]
+ * 提示：
+ *
+ * 链表长度在[0, 20000]范围内。
+ * 链表元素在[0, 20000]范围内。
+ * 进阶：
+ *
+ * 如果不得使用临时缓冲区，该怎么解决？
+ *
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicate-node-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingda
+ */
+public class M0201 {
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(3);
+        ListNode n5 = new ListNode(2);
+        ListNode n6 = new ListNode(1);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        ListNode node = removeDuplicateNodes(n6);
+        System.out.println(node);
+
+
+    }
+
+
+    public static ListNode removeDuplicateNodes(ListNode head) {
+        Set<Integer> set = new HashSet<>();
+        ListNode root = new ListNode(0);
+        ListNode re = root;
+        root.next = head;
+        while (root.next != null) {
+            if (set.contains(root.next.val)) {
+                root.next = root.next.next;
+            } else {
+                set.add(root.next.val);
+                root = root.next;
+            }
+        }
+        return re.next;
+    }
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+}
