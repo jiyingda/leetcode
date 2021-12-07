69cd8c49ee7b76a923ef5b19955f6de132f1db0a
jiyingdabj <jiyingdabj@yuanfudao.com>
2021-12-07 23:02:02 +0800

    ADD: offer095

---
+++ src/main/java/com/jiyingda/ms/M0206.java
@@ -0,0 +1,36 @@
+/**
+ * @(#)M0206.java, 10月 22, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.ms;
+
+/**
+ * @author jiyingda
+ */
+public class M0206 {
+
+
+    public boolean isPalindrome(ListNode head) {
+        int i = 0, j = 0;
+        while (head != null) {
+            i++;
+            j++;
+            head = head.next;
+            if (head == null) {
+                break;
+            }
+            head = head.next;
+            j++;
+        }
+
+        return false;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
---
+++ src/main/java/com/jiyingda/offer/Offer095.java
@@ -0,0 +1,61 @@
+/**
+ * @(#)Offer095.java, 12月 07, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.offer;
+
+/**
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ *
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ *
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：text1 = "abcde", text2 = "ace"
+ * 输出：3
+ * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+ * 示例 2：
+ *
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc" ，它的长度为 3 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/qJnOS7
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingda
+ */
+public class Offer095 {
+
+    public static void main(String[] args) {
+        int a = longestCommonSubsequence("abcde", "ace");
+        System.out.print(a);
+
+
+    }
+
+    public static int longestCommonSubsequence(String text1, String text2) {
+        char[] array1 = text1.toCharArray();
+        char[] array2 = text2.toCharArray();
+        int m = array1.length;
+        int n = array2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (array1[i-1] == array2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
