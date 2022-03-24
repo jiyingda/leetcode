4d95d3f7beef68bea580ca6403a33604a895c190
jiyingdabj <jiyingdabj@yuanfudao.com>
2022-03-24 14:59:28 +0800

    637

---
+++ src/main/java/com/jiyingda/ms/StreamRank.java
@@ -0,0 +1,29 @@
+/**
+ * @(#)StreamRank.java, 3月 23, 2022.
+ * <p>
+ * Copyright 2022 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.ms;
+
+/**
+ * @author jiyingdabj
+ */
+public class StreamRank {
+    int[] nums;
+    public StreamRank() {
+        nums = new int[50001];
+    }
+
+    public void track(int x) {
+        nums[x]++;
+    }
+
+    public int getRankOfNumber(int x) {
+        int all = 0;
+        for (int i = 0; i <= x; i++) {
+            all += nums[i];
+        }
+        return all;
+    }
+}
