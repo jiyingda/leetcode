6dfb1727e446b5f12953483156c3a875898ca195
jiyingdabj <jiyingdabj@yuanfudao.com>
2022-06-30 18:01:38 +0800

    1

--- src/main/java/com/jiyingda/redo/Solution3.java
+++ src/main/java/com/jiyingda/redo/Solution3.java
@@ -6,8 +6,70 @@
  */
 package com.jiyingda.redo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ *
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= s.length <= 5 * 104
+ * s 由英文字母、数字、符号和空格组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author jiyingdabj
  */
 public class Solution3 {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring(""));;
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        char[] ss = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        int i = 0, j = 0;
+        while (j < ss.length) {
+            if (!map.containsKey(ss[j])) {
+                map.put(ss[j], j);
+                j++;
+            } else {
+                max = Math.max(max, j - i);
+                int k = map.get(ss[j]);
+                while (i <= k) {
+                    map.remove(ss[i]);
+                    i++;
+                }
+            }
+        }
+        if (i < j) {
+            max = Math.max(max, j - i);
+        }
+        return max;
+    }
 }
---
+++ src/main/java/com/jiyingda/redo/Solution6.java
@@ -0,0 +1,91 @@
+/**
+ * @(#)Solution6.java, 6月 30, 2022.
+ * <p>
+ * Copyright 2022 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.redo;
+
+import java.util.Locale;
+
+/**
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ *
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ *
+ * string convert(string s, int numRows);
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "PAYPALISHIRING", numRows = 3
+ * 输出："PAHNAPLSIIGYIR"
+ * 示例 2：
+ * 输入：s = "PAYPALISHIRING", numRows = 4
+ * 输出："PINALSIGYAHRPI"
+ * 解释：
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * 示例 3：
+ *
+ * 输入：s = "A", numRows = 1
+ * 输出："A"
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 1000
+ * s 由英文字母（小写和大写）、',' 和 '.' 组成
+ * 1 <= numRows <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/zigzag-conversion
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingdabj
+ */
+public class Solution6 {
+
+    public static void main(String[] args) {
+        String s = convert("ab", 1);
+        String k = "PAHNAPLSIIGYIR";
+        System.out.println(s);
+        System.out.println(k.equals(s));
+    }
+
+
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder[] sbs = new StringBuilder[numRows];
+        for (int i = 0; i < sbs.length; i++) {
+            sbs[i] = new StringBuilder();
+        }
+        char[] ss = s.toCharArray();
+        int pre = 1;
+        int a = 0;
+        for (char c : ss) {
+            sbs[a].append(c);
+            a = a + pre;
+            if (a <= 0) {
+                pre = 1;
+            } else if (a >= numRows - 1) {
+                pre = -1;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (StringBuilder b : sbs) {
+            sb.append(b);
+        }
+        return sb.toString();
+    }
+}
