35ae83beee9ae44e5514af932d8ff494ca671019
jiyingdabj <jiyingdabj@yuanfudao.com>
2022-07-06 16:28:39 +0800

    ADD: 977

--- src/main/java/com/jiyingda/leetcode200/Leet189.java
+++ src/main/java/com/jiyingda/leetcode200/Leet189.java
@@ -47,9 +47,7 @@ public class Leet189 {
         for (int i = 0; i < nums.length; i++) {
             a[(i + k) % nums.length] = nums[i];
         }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = a[i];
-        }
+        System.arraycopy(a, 0, nums, 0, nums.length);
     }
 
 }
---
+++ src/main/java/com/jiyingda/util/PrintUtils.java
@@ -0,0 +1,19 @@
+/**
+ * @(#)PrintUtils.java, 7月 06, 2022.
+ * <p>
+ * Copyright 2022 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.util;
+
+/**
+ * @author jiyingdabj
+ */
+public class PrintUtils {
+
+    public static void printArray(int[] a) {
+        for (int b : a) {
+            System.out.print(b + "\t");
+        }
+    }
+}
