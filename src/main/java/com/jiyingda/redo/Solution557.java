ab842dd0375770b62b2da3f3b4a4ad34933e77fc
jiyingdabj <jiyingdabj@yuanfudao.com>
2022-07-11 18:43:32 +0800

    557

---
+++ src/main/java/com/jiyingda/redo/Solution557.java
@@ -0,0 +1,67 @@
+/**
+ * @(#)Solution557.java, 7月 11, 2022.
+ * <p>
+ * Copyright 2022 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.redo;
+
+/**
+ * 给定一个字符串 s ，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "Let's take LeetCode contest"
+ * 输出："s'teL ekat edoCteeL tsetnoc"
+ * 示例 2:
+ *
+ * 输入： s = "God Ding"
+ * 输出："doG gniD"
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 5 * 104
+ * s 包含可打印的 ASCII 字符。
+ * s 不包含任何开头或结尾空格。
+ * s 里 至少 有一个词。
+ * s 中的所有单词都用一个空格隔开。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/reverse-words-in-a-string-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingdabj
+ */
+public class Solution557 {
+
+
+    public String reverseWords(String s) {
+        String[] ss = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ss.length - 2; i++) {
+            reverseString(ss[i].toCharArray(), sb);
+            sb.append(" ");
+        }
+        reverseString(ss[ss.length - 1].toCharArray(), sb);
+        return sb.toString();
+    }
+
+    public void reverseString(char[] s, StringBuilder sb) {
+        int j = s.length - 1;
+        int i = 0;
+        while (i < j) {
+            swap(s, i, j);
+            i++;
+            j--;
+        }
+        sb.append(s);
+    }
+
+    public void swap(char[] s, int i, int j) {
+        char t = s[i];
+        s[i] = s[j];
+        s[j] = t;
+    }
+}
---
+++ src/main/java/com/jiyingda/redo/Solution83.java
@@ -0,0 +1,63 @@
+/**
+ * @(#)Solution83.java, 7月 11, 2022.
+ * <p>
+ * Copyright 2022 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.redo;
+
+import com.jiyingda.entity.ListNode;
+
+/**
+ * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ *  
+ *
+ * 提示：
+ *
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序 排列
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/remove-duplicates-from-sorted-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingdabj
+ */
+public class Solution83 {
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(1);
+        ListNode n3 = new ListNode(2);
+        n1.next = n2;
+        n2.next = n3;
+        deleteDuplicates(n1);
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode re = head;
+        int pre = head.val;
+        while (head.next != null) {
+            if (head.next.val == pre) {
+                head.next = head.next.next;
+            } else {
+                pre = head.next.val;
+                head = head.next;
+            }
+        }
+        return re;
+    }
+}
