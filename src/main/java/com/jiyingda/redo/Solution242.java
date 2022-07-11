883ca79186b408e15feead6001e673dbcef37670
jiyingdabj <jiyingdabj@yuanfudao.com>
2022-07-11 17:08:28 +0800

    242

---
+++ src/main/java/com/jiyingda/redo/Solution242.java
@@ -0,0 +1,56 @@
+/**
+ * @(#)Solution242.java, 7月 11, 2022.
+ * <p>
+ * Copyright 2022 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.redo;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *  
+ *
+ * 提示:
+ *
+ * 1 <= s.length, t.length <= 5 * 104
+ * s 和 t 仅包含小写字母
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/valid-anagram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingdabj
+ */
+public class Solution242 {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] ch = new int[26];
+        for (char c : s.toCharArray()) {
+            ch[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            ch[c - 'a']--;
+            if (ch[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/com/jiyingda/redo/Solution387.java
@@ -0,0 +1,56 @@
+/**
+ * @(#)Solution387.java, 7月 11, 2022.
+ * <p>
+ * Copyright 2022 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.redo;
+
+/**
+ * 给定一个字符串 s ，找到 它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入: s = "leetcode"
+ * 输出: 0
+ * 示例 2:
+ *
+ * 输入: s = "loveleetcode"
+ * 输出: 2
+ * 示例 3:
+ *
+ * 输入: s = "aabb"
+ * 输出: -1
+ *  
+ *
+ * 提示:
+ *
+ * 1 <= s.length <= 105
+ * s 只包含小写字母
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/first-unique-character-in-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author jiyingdabj
+ */
+public class Solution387 {
+
+    public int firstUniqChar(String s) {
+        char[] array = s.toCharArray();
+        int[] ch = new int[26];
+        for (char c : array) {
+            int idx = c - 'a';
+            ch[idx]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            int idx = array[i] - 'a';
+            if (ch[idx] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
