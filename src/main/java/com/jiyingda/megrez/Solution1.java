1feaa670e5b7a5e8673682b28c4317fbb3313489
jiyingdabj <jiyingdabj@yuanfudao.com>
2022-07-14 11:36:42 +0800

    s1

--- src/main/java/com/jiyingda/megrez/Solution1.java
+++ src/main/java/com/jiyingda/megrez/Solution1.java
@@ -6,7 +6,11 @@
  */
 package com.jiyingda.megrez;
 
+import com.jiyingda.util.PrintUtils;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 练习第一期
@@ -28,60 +32,103 @@ public class Solution1 {
     static int[] allCards = new int[]{1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,21,22,23,24,25,26,27,28,29};
 
     public static void main(String[] args) {
-        int[] cards = new int[]{2,3,4,4,5,6,7,8,8,8,9,9,11};
-        boolean f = check13(cards);
-        System.out.print(f);
+        int[] cards = new int[]{3,3,3,4,4,4,5,5,5,12,12,12,13};
+        List<Integer> tp = check13(cards);
+        PrintUtils.printArray(tp);
     }
 
     /**
      * 判断是否听牌
      */
-    public static boolean check13(int[] cards) {
+    public static List<Integer> check13(int[] cards) {
+        List<Integer> tinPais = new ArrayList<>();
         int[] pai = new int[30];
         for (int c : cards) {
             pai[c]++;
         }
-        pai[10] = -1;
-        pai[20] = -1;
         for (int other : allCards) {
-            if (pai[other] < 4) {
+            if (pai[other] >= 0 && pai[other] < 4) {
                 int[] temp = Arrays.copyOf(pai, pai.length);
                 temp[other]++;
                 boolean f = check14(temp);
                 if (f) {
-                    return true;
+                    // 保存所有可能听的牌
+                    tinPais.add(other);
                 }
             }
         }
-        return false;
+        return tinPais;
     }
 
     /**
      * 判断是否胡牌
      */
     public static boolean check14(int[] pai) {
-        int i = 1;
-        while (i < 30) {
-            if (pai[i] >= 3) {
-                pai[i] = pai[i] - 3;
-            } else if (pai[i] == 1) {
-                if (i + 2 < 30 && pai[i + 1] > 0 && pai[i + 2] > 0) {
-                    pai[i]--;
-                    pai[i + 1]--;
-                    pai[i + 2]--;
-                    i++;
-                } else {
-                    return false;
-                }
-            } else if (pai[i] == 2) {
-                pai[i] = pai[i] - 2;
-                i++;
-            } else if (pai[i] < 0) {
-                return false;
-            } else {
-                i++;
-            }
+        // 利用回溯来处理多种情况的判断
+        return dfs(pai, 1, 4, 1);
+    }
+
+    /**
+     * 递归
+     * c3 刻/顺的个数
+     * c2 对的数量
+     */
+    public static boolean dfs(int[] pai, int i, int c3, int c2) {
+        int len = pai.length;
+        while (i < len && pai[i] == 0) {
+            i++;
         }
-        return true;
+        if (i >= len) {
+            return c3 == 0 && c2 == 0;
+        }
+        if (pai[i] < 0 || c3 < 0 || c2 < 0) {
+            return false;
+        }
+        // pai[i] == 1 必然要连顺
+        if (pai[i] == 1) {
+            // 后面的牌不够连顺
+            if (i + 2 >= len || pai[i + 1] < 1 || pai[i + 2] < 1) {
+                return false;
+            }
+            // 能连成顺则 i + 1 继续判断
+            pai[i]--;
+            pai[i + 1]--;
+            pai[i + 2]--;
+            return dfs(pai, i + 1, c3 - 1, c2);
+        }
+        // pai[i] == 2 可以选择对 也可以选择2条顺
+        if (pai[i] == 2) {
+            // 先选择对，如果不能胡的话，则进行回溯
+            pai[i] = pai[i] - 2;
+            boolean f = dfs(pai, i + 1, c3, c2 - 1);
+            if (f) {
+                return true;
+            }
+            // 回溯
+            pai[i] = pai[i] + 2;
+            // 选择2条顺 这边也可以先选择一条顺 不过会再进入到 pai[i] = 1 的情况 实质是一样的
+            if (i + 2 >= len || pai[i + 1] < 2 || pai[i + 2] < 2) {
+                // 后面的牌不够2个顺
+                return false;
+            }
+            pai[i] = pai[i] - 2;
+            pai[i + 1] = pai[i + 1] - 2;
+            pai[i + 2] = pai[i + 2] - 2;
+            return dfs(pai, i + 1, c3 - 2, c2);
+        }
+        // pai[i] >= 3 也有2种情况 1,2,3,3,3,3,4,5这种情况会在前面判断pai[i] == 1的时候处理掉了
+        // 我们只需考虑 选择刻还是对
+        // 这边i不加1 因为pi[i]可能还没用完 让程序接下来判断 pai[i] == 0/1/2 的情况
+        pai[i] = pai[i] - 3;
+        boolean f =  dfs(pai, i, c3 - 1, c2);
+        if (f) {
+            return true;
+        }
+        // 进行回溯
+        pai[i] = pai[i] + 3;
+        // 选择对子 这边不可能再出现3条顺的情况 因为等价于3个刻
+        pai[i] = pai[i] - 2;
+        // i不用加1
+        return dfs(pai, i, c3, c2 - 1);
     }
 }
