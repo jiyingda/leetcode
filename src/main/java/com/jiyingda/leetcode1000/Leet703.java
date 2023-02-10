be9829e418d9bf21f05151addbccb63bdc2572b2
jiyingdabj <jiyingdabj@yuanfudao.com>
2023-02-10 14:58:34 +0800

    ADD: Leet703

---
+++ src/main/java/com/jiyingda/leetcode1000/Leet703.java
@@ -0,0 +1,88 @@
+/**
+ * @(#)Leet703.java, 2月 10, 2023.
+ * <p>
+ * Copyright 2023 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.leetcode1000;
+
+import java.util.PriorityQueue;
+
+/**
+ * 设计一个找到数据流中第 k 大元素的类（class）。注意是排序后的第 k 大元素，不是第 k 个不同的元素。
+ *
+ * 请实现 KthLargest 类：
+ *
+ * KthLargest(int k, int[] nums) 使用整数 k 和整数流 nums 初始化对象。
+ * int add(int val) 将 val 插入数据流 nums 后，返回当前数据流中第 k 大的元素。
+ *
+ *
+ * 示例：
+ *
+ * 输入：
+ * ["KthLargest", "add", "add", "add", "add", "add"]
+ * [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+ * 输出：
+ * [null, 4, 5, 5, 8, 8]
+ *
+ * 解释：
+ * KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+ * kthLargest.add(3);   // return 4
+ * kthLargest.add(5);   // return 5
+ * kthLargest.add(10);  // return 5
+ * kthLargest.add(9);   // return 8
+ * kthLargest.add(4);   // return 8
+ *
+ *
+ * 提示：
+ * 1 <= k <= 104
+ * 0 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * -104 <= val <= 104
+ * 最多调用 add 方法 104 次
+ * 题目数据保证，在查找第 k 大元素时，数组中至少有 k 个元素
+ * @author jiyingdabj
+ */
+public class Leet703 {
+
+    public static void main(String[] args) {
+        Leet703 kthLargest = new Leet703(3, new int[]{4, 5, 8, 2});
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+        System.out.println(kthLargest.add(9));
+        System.out.println(kthLargest.add(4));
+        System.out.println(kthLargest);
+
+    }
+
+    private PriorityQueue<Integer> queue;
+    private int kk;
+    public Leet703(int k, int[] nums) {
+        queue = new PriorityQueue<>(k);
+        kk = k;
+        int i = 0;
+        while (i < k && i < nums.length) {
+            queue.add(nums[i]);
+            i++;
+        }
+        while (i < nums.length) {
+            add(nums[i]);
+            i++;
+        }
+
+    }
+
+    public int add(int val) {
+        if (queue.size() < kk) {
+            queue.add(val);
+        } else {
+            int t = queue.peek();
+            if (val > t) {
+                queue.poll();
+                queue.add(val);
+            }
+        }
+        return queue.peek();
+    }
+}
---
+++ src/main/java/com/jiyingda/leetcode500/Leet384.java
@@ -0,0 +1,87 @@
+/**
+ * @(#)Leet384.java, 2月 03, 2023.
+ * <p>
+ * Copyright 2023 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.leetcode500;
+
+import com.jiyingda.util.PrintUtils;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 给你一个整数数组 nums ，设计算法来打乱一个没有重复元素的数组。打乱后，数组的所有排列应该是 等可能 的。
+ *
+ * 实现 Solution class:
+ *
+ * Solution(int[] nums) 使用整数数组 nums 初始化对象
+ * int[] reset() 重设数组到它的初始状态并返回
+ * int[] shuffle() 返回数组随机打乱后的结果
+ *
+ *
+ * 示例 1：
+ *
+ * 输入
+ * ["Solution", "shuffle", "reset", "shuffle"]
+ * [[[1, 2, 3]], [], [], []]
+ * 输出
+ * [null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+ *
+ * 解释
+ * Solution solution = new Solution([1, 2, 3]);
+ * solution.shuffle();    // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。例如，返回 [3, 1, 2]
+ * solution.reset();      // 重设数组到它的初始状态 [1, 2, 3] 。返回 [1, 2, 3]
+ * solution.shuffle();    // 随机返回数组 [1, 2, 3] 打乱后的结果。例如，返回 [1, 3, 2]
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 50
+ * -106 <= nums[i] <= 106
+ * nums 中的所有元素都是 唯一的
+ * 最多可以调用 104 次 reset 和 shuffle
+ * @author jiyingdabj
+ */
+public class Leet384 {
+
+    private int[] nums;
+    private int len;
+    private Random random = new Random();
+
+    public Leet384(int[] nums) {
+        len = nums.length;
+        this.nums = nums;
+
+    }
+
+    public int[] reset() {
+        return this.nums;
+    }
+
+    public int[] shuffle() {
+        int[] shuffleNums = Arrays.copyOf(nums, len);
+
+        for (int i = 0; i < len; i++) {
+            int j = random.nextInt(len);
+            swap(shuffleNums, i, j);
+        }
+        return shuffleNums;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int n = nums[i];
+        nums[i] = nums[j];
+        nums[j] = n;
+    }
+
+    public static void main(String[] args) {
+        Leet384 leet384 = new Leet384(new int[]{1,2,3});
+        int[] shuff = leet384.shuffle();
+        //PrintUtils.printArray(shuff);
+        int[] ss = leet384.reset();
+        PrintUtils.printArray(ss);
+
+    }
+}
