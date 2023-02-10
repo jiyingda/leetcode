bb07cf80aa19012b904cfe08250e147675de0e20
jiyingdabj <jiyingdabj@yuanfudao.com>
2023-02-10 18:17:26 +0800

    872

---
+++ src/main/java/com/jiyingda/leetcode1000/Leet872.java
@@ -0,0 +1,101 @@
+/**
+ * @(#)Leet872.java, 2月 10, 2023.
+ * <p>
+ * Copyright 2023 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.leetcode1000;
+
+import com.jiyingda.util.PrintUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ *
+ *
+ *
+ * 举个例子，如上图所示，给定一棵叶值序列为 (6, 7, 4, 9, 8) 的树。
+ *
+ * 如果有两棵二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
+ *
+ * 如果给定的两个根结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+ * 输出：true
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：root1 = [1,2,3], root2 = [1,3,2]
+ * 输出：false
+ *
+ *
+ * 提示：
+ *
+ * 给定的两棵树结点数在 [1, 200] 范围内
+ * 给定的两棵树上的值在 [0, 200] 范围内
+ * 通过次数
+ * 70.1K
+ * 提交次数
+ * 107.7K
+ * 通过率
+ * 65.1%
+ * @author jiyingdabj
+ */
+public class Leet872 {
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        n1.left = n2;
+        n1.right = n3;
+        n3.right = n4;
+        List<Integer> list = findLeaf(n1);
+        PrintUtils.printArray(list);
+
+    }
+
+    public static boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = findLeaf(root1);
+        List<Integer> l2 = findLeaf(root2);
+        if (l1.size() != l2.size()) {
+            return false;
+        }
+        for (int i = 0; i < l1.size(); i++) {
+            if (!l1.get(i).equals(l2.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<Integer> findLeaf(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root, list);
+        return list;
+    }
+
+
+    public static void dfs(TreeNode root, List<Integer> list) {
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+            return;
+        }
+        if (root.left != null) {
+            dfs(root.left, list);
+        }
+        if (root.right != null) {
+            dfs(root.right, list);
+        }
+    }
+}
