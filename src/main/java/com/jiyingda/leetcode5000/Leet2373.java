be68407639a425d97f00cfc646dbe2ea9831f817
jiyingdabj <jiyingdabj@yuanfudao.com>
2023-03-01 16:59:48 +0800

    2373

---
+++ src/main/java/com/jiyingda/leetcode5000/Leet2373.java
@@ -0,0 +1,76 @@
+/**
+ * @(#)Leet2373.java, 3月 01, 2023.
+ * <p>
+ * Copyright 2023 yuanfudao.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jiyingda.leetcode5000;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 给你一个大小为 n x n 的整数矩阵 grid 。
+ *
+ * 生成一个大小为 (n - 2) x (n - 2) 的整数矩阵  maxLocal ，并满足：
+ *
+ * maxLocal[i][j] 等于 grid 中以 i + 1 行和 j + 1 列为中心的 3 x 3 矩阵中的 最大值 。
+ * 换句话说，我们希望找出 grid 中每个 3 x 3 矩阵中的最大值。
+ *
+ * 返回生成的矩阵。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+ * 输出：[[9,9],[8,6]]
+ * 解释：原矩阵和生成的矩阵如上图所示。
+ * 注意，生成的矩阵中，每个值都对应 grid 中一个相接的 3 x 3 矩阵的最大值。
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：grid = [[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]]
+ * 输出：[[2,2,2],[2,2,2],[2,2,2]]
+ * 解释：注意，2 包含在 grid 中每个 3 x 3 的矩阵中。
+ *
+ *
+ * 提示：
+ *
+ * n == grid.length == grid[i].length
+ * 3 <= n <= 100
+ * 1 <= grid[i][j] <= 100
+ *
+ * https://leetcode.cn/problems/largest-local-values-in-a-matrix/
+ *
+ * @author jiyingdabj
+ */
+public class Leet2373 {
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{
+                {9,9,8,1},{5,6,2,6},{8,2,6,4},{6,2,2,2}
+        };
+        int[][] re = largestLocal(grid);
+        System.out.println(re);
+
+    }
+
+    public static int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] res = new int[n - 2][n - 2];
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                for (int x = i; x < i + 3; x++) {
+                    for (int y = j; y < j + 3; y++) {
+                        res[i][j] = Math.max(res[i][j], grid[x][y]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
