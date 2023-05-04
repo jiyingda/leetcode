27751af49921f5d77df048416ae5a481ad22e982
jiyingda <610181296@qq.com>
2023-05-04 22:57:59 +0800

    970

--- src/main/java/com/jiyingda/sort/QuickSort.java
+++ src/main/java/com/jiyingda/sort/QuickSort.java
@@ -1,11 +1,5 @@
 package com.jiyingda.sort;
 
-import sun.jvm.hotspot.utilities.BitMap;
-
-import java.util.BitSet;
-import java.util.Random;
-import java.util.concurrent.CountDownLatch;
-
 /**
  * @author jiyin
  * @date 2020/8/2 8:02
@@ -13,22 +7,8 @@ import java.util.concurrent.CountDownLatch;
 public class QuickSort {
     public static void main(String[] args) {
 
-        BitSet set = new BitSet();
-
-        BitMap bitMap = new BitMap(10);
-        bitMap.atPut(2, true);
-        System.out.println(bitMap.at(1));
 
         int[] arr = new int[]{19,5,4,6,7,3,9,2,1,8, 100};
-        for (int i : arr) {
-            set.set(i);
-        }
-        System.out.println("size = " + set.size());
-        for (int i = 0; i < set.size(); i++) {
-            if (set.get(i)) {
-                System.out.println(i);
-            }
-        }
         //quickSort(arr);
         quickSort2(arr);
         for(int a : arr){
