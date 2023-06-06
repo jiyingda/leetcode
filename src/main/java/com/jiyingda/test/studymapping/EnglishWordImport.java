71b1a6f7777ab2696ac4ebbe18c466a8f2c7a6bb
jiyingdabj <jiyingdabj@yuanfudao.com>
2023-06-06 15:09:29 +0800

    1093

--- src/main/java/com/jiyingda/test/studymapping/EnglishWordImport.java
+++ src/main/java/com/jiyingda/test/studymapping/EnglishWordImport.java
@@ -15,13 +15,29 @@ import java.util.List;
  */
 public class EnglishWordImport {
 
+    private static int getWordType(String c) {
+        if ("核心词汇".equals(c)) {
+            return 1;
+        }
+        if ("其他词汇".equals(c)) {
+            return 2;
+        }
+        if ("必会词汇".equals(c)) {
+            return 3;
+        }
+        throw new RuntimeException();
+
+
+    }
+
     public static void main(String[] args) {
         // List<String> list = FileReader.readFromVs("牛津上海版单词数据");
         // List<String> list = FileReader.readFromVs("牛津上海版单词数据222");
-        List<String> list = FileReader.readFromVs("沪教牛津三年级起点单词导入");
+        //List<String> list = FileReader.readFromVs("沪教牛津三年级起点单词导入");
+        List<String> list = FileReader.readFromVs("外研社版三起点英语单词导入");
         for (String s : list) {
             String[] ss = s.split("\t");
-            int a12 = "核心词汇".equals(ss[12]) ? 1 : 2;
+            int a12 = getWordType(ss[12].trim());
             if (ss[9].contains("'")) {
                 //System.out.println(s);
             }
