c8aa618b8ce8b2355a097dc0dce9aa174c2cb546
jiyingdabj <jiyingdabj@yuanfudao.com>
2024-02-06 16:46:24 +0800

    ADD: 0206

--- src/main/java/com/jiyingda/test/SortTest.java
+++ src/main/java/com/jiyingda/test/SortTest.java
@@ -15,8 +15,10 @@ public class SortTest {
 
     public static void main(String[] args) {
         int[] nums = {3, 2, 1, 5, 6, 4};
-        solution(nums);
-        PrintUtils.printArray(nums);
+        // solution(nums);
+        //PrintUtils.printArray(nums);
+        int a = findKthLargest(nums, 6);
+        System.out.println(a);
     }
 
     private static void solution(int[] nums) {
@@ -51,5 +53,38 @@ public class SortTest {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        return findKthLargest(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    private static int findKthLargest(int[] nums, int left, int right, int k) {
+        if (left >= right) {
+            return nums[left];
+        }
+        int target = nums[left];
+        int i = left;
+        int j = right;
+        while (i < j) {
+            while (nums[j] >= target && i < j) {
+                j--;
+            }
+            while (nums[i] <= target && i < j) {
+                i++;
+            }
+            if (i < j) {
+                swap(nums, i, j);
+            }
+        }
+        nums[left] = nums[i];
+        nums[i] = target;
+        if (i == k) {
+           return nums[i];
+        } else if (i > k) {
+            return findKthLargest(nums, left, i - 1, k);
+        } else {
+            return findKthLargest(nums, i + 1, right, k);
+        }
     }
 }
